129d85f9da0c59dadff801e2d4e2f0563817ebb0
nyd-12 <nyd@outlook.com>
2024-04-19 22:38:07 +0530

    LAST LELO

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+import Location.Country;
+import Location.Hospital;
+import Location.State;
+import Subject.Patient;
+import utility.all_data;
+
+public class Main {
+    public static void main(String[] args) {
+
+        all_data data = new all_data();
+        Country country = data.getCountry();
+        data.readInputFromFile("inputFiles/input.txt");
+        data.readInputFromFile("inputFiles/hospital.txt", country);
+        Patient[] patients = data.getPatients();
+        Hospital[] hospitals = data.getHospitals();
+        State[] states = data.getStates();
+
+        System.out.println("Names of Patients:");
+        for (Patient patient : patients) {
+            if (patient != null) {
+                System.out.println(patient.getName());
+            }
+        }
+
+        System.out.println("\n\nNames of Hospitals:");
+        for (Hospital hospital : hospitals) {
+            if (hospital != null) {
+                System.out.println(hospital.getName());
+            }
+        }
+
+        System.out.println("\n\nNames of States:");
+        for (State state : states) {
+            if (state != null) {
+                System.out.println(state.getName());
+            }
+        }
+        int i = 0;
+        int j = 0;
+        // main logic to put patient in hospital
+        while(i < hospitals.length)
+        {
+            //System.out.println("LAST KAAM BAAKI");
+            while(j<patients.length)
+            {
+            if(patients[j] == null || hospitals[i] == null)
+            {
+                break;
+            }
+            patients[j].isConfirmedCase();
+            if(patients[j].getStatus().equals("Infected") && !patients[j].getAdmitStatus())
+            {
+                if(hospitals[i].addSubObj(patients[j])){
+                //System.out.println("ADMIT KIYA???");
+                //hospitals[i].addPatient(patients[j]);
+                patients[j].setAdmitStatus(true);
+                j++;
+                }
+                else{
+                break;
+                }
+            }
+            }
+            i++;
+        }
+        country.displayData();
+        // Method to read input from file and create patients and contacts
+    }
+    
+}
---
+++ src/utility/Display.java
@@ -0,0 +1,8 @@
+package utility;
+/**
+ * Displayable
+ */
+public interface Display {
+
+    void displayData();
+}
